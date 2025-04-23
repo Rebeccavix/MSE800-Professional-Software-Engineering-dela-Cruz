d87c8e7d2375aedc6e562316eb44a9e1932af67d
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-04-23 21:09:01 +1200

    done file organizing

--- Week1/1-Activity/PositiveInteger.py
+++
@@ -1,5 +0,0 @@
-#Activity : Create a NumPy array of the first 10 positive integers
-# 1.Print the array.
-# 2.Print the shape and data type of the array.
-# 3.Multiply each element by 2 and print the result.
-
