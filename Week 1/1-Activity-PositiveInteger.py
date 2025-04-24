6897a10ce111d4e940bf55301acf74ebdd0bb2b9
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-04-24 14:04:02 +1200

    Organized files and done 1-Activity

--- Week 1/1-Activity-PositiveInteger.py
+++ Week 1/1-Activity-PositiveInteger.py
@@ -1,13 +1,14 @@
 #1- Activity : Create a NumPy array of the first 10 positive integers. Then:
-#Print the array.
-#Print the shape and data type of the array.
-#Multiply each element by 2 and print the result.
+#   1. Print the array.
+#   2. Print the shape and data type of the array.
+#   3. Multiply each element by 2 and print the result.
  
 import numpy as np
 
+arr = np.arange(1,2,3,4,5,6,7,8,8,9,10)
 # 1. Create a NumPy array of the first 10 positive integers
-arr = np.arange(1, 11)
-print("Original array:")
+# 1. Print the array
+print("array:")
 print(arr)
 
 # 2. Print the shape and data type of the array
