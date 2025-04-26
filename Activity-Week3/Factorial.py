994aab9bb5da99c7850bd5d289d19faa8e1ae531
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-04-26 12:54:02 +1200

    loop

--- Activity-Week3/Factorial.py
+++ Activity-Week3/Factorial.py
@@ -5,8 +5,11 @@ def factorial(n):
         return n * factorial(n-1)
 
 # Examples
-print("0! =", factorial(0))
-print("1! =", factorial(1))
-print("2! =", factorial(2))
-print("3! =", factorial(3))
-print("4! =", factorial(4))
+#print("0! =", factorial(0))
+#print("1! =", factorial(1))
+#print("2! =", factorial(2))
+#print("3! =", factorial(3))
+#print("4! =", factorial(4))
+
+for i in range(5):  # Range starts from 0 and ends at 4
+    print(f"{i}! =", factorial(i))
