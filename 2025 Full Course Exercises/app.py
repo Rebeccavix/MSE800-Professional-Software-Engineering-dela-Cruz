178ceaf6eb6e7411f52c6d7cbca57e1e209d2899
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-05-01 09:47:46 +1200

    Update app.py

--- 2025 Full Course Exercises/app.py
+++ 2025 Full Course Exercises/app.py
@@ -1,1 +1,12 @@
-2+
+number = 0
+x = 1
+a = input("a: ")
+b = input("b: ")
+for i in range(a, b):
+    if i % 2 == 0:
+        print(i)
+        number += 1
+print(f"There are {number} even numbers from {a} to {b}")
+x = 1
+x = 1
+x = 1
