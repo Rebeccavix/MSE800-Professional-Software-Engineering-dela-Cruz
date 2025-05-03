78aac68f629ef352890d224943011e4511d851d1
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-05-04 08:42:13 +1200

    Activity 2 Parquet Database

--- Activity-Week4/2-1-Activity_ReadandPrintOut.py
+++ Activity-Week4/2-1-Activity_ReadandPrintOut.py
@@ -5,5 +5,5 @@
 with open('Activity-Week4\sample_text.txt', 'r') as file:
     lines = file.readlines()
     if lines:
-        print("First line:", lines[0].strip())
-        print("Last line:", lines[-1].strip())
+        print("First line:", lines[0])
+        print("Last line:", lines[-1])
