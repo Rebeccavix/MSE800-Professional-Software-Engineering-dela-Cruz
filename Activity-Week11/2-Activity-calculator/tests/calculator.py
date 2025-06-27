bdc97c75ed86dedd51b6f54be83aae2033ac37cb
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-06-28 11:07:17 +1200

    Week 11 - Activity 2

--- Activity-Week11/2-Activity-calculator/tests/calculator.py
+++ Activity-Week11/2-Activity-calculator/tests/calculator.py
@@ -5,21 +5,14 @@
 import math
 
 
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
+def add(a, b): return a + b
+def subtract(a, b): return a - b
+def multiply(a, b): return a * b
 
 
 def divide(a, b):
     if b == 0:
-        raise ValueError("Cannot divide by zero")
+        raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
 
 
