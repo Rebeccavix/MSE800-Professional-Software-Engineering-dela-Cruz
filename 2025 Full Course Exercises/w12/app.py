6b264874974c20e54ccf61403cb0ab212615dd2d
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-06-28 21:39:31 +1200

    test

--- 2025 Full Course Exercises/w12/app.py
+++ 2025 Full Course Exercises/w12/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static')
+
+# Make sure the upload folder exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 
 @app.route("/")
@@ -14,30 +18,22 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/profile")
+@app.route("/profile", methods=['GET', 'POST'])
 def profile():
-    return render_template("profile.html")
-
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static')
-
-
-@app.route('/static', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filename = file.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        # url_for('static', filename=filename) will generate the correct URL for the static file
-        image_url = url_for('static', filename=filename)
-        return render_template('profile.html', image_url=image_url)
-    return redirect(request.url)
+    image_url = None
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            # For production, use secure_filename!
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            image_url = url_for('static', filename=filename)
+    return render_template('profile.html', image_url=image_url)
 
 
 if __name__ == "__main__":
