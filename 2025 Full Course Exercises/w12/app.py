c49f0133e186cfb76554ae2a97b00fc4b903dbe5
vkweb8 <50472462+vkweb8@users.noreply.github.com>
2025-06-28 21:44:04 +1200

    test

--- 2025 Full Course Exercises/w12/app.py
+++ 2025 Full Course Exercises/w12/app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static')
-
-# Make sure the upload folder exists
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 
 @app.route("/")
@@ -18,22 +14,28 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/profile", methods=['GET', 'POST'])
+@app.route("/profile")
 def profile():
-    image_url = None
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file:
-            # For production, use secure_filename!
-            filename = file.filename
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            image_url = url_for('static', filename=filename)
-    return render_template('profile.html', image_url=image_url)
+    return render_template("profile.html")
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        image_url = url_for('static', filename='uploads/' + file.filename)
+        return render_template('profile.html', image_url=image_url)
 
 
 if __name__ == "__main__":
